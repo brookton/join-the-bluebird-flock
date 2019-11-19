bd73dbbb70a35c9f532ae4565e458fea49ea4e34
Brookton <davidbrookton@gmail.com>
2019-11-18 22:53:47 -0700

    :heart: Added love

--- src/components/First.js
+++ src/components/First.js
@@ -5,14 +5,17 @@ import './First.css'
 function First() {
   return (
     <div className="First">
-      <LazyHero  opacity="0" minHeight="600px"  parallaxOffset="0" imageSrc={ require('../images/hemp-classic.jpg') } isCentered="true" >
+      <LazyHero  opacity="0" minHeight="600px"  parallaxOffset="0" imageSrc={ require('../images/hemp-classic.jpg') } className="hero-first" isCentered="true" >
         <div className="flex-container">
+          <div className="mobile-image">
+            <img src={ require('../images/hemp-classic-mobile.png') }/>
+          </div>
           <div className="copy">
               <h1>This is your </h1>
               <h1>first step.</h1> 
-            <button type="button"  class="btn btn-primary">Shop CBD Now</button>
+            <button type="button" onclick="window.location.href='/page2'" >Shop CBD Now</button>
           </div>
-        </div>
+        </div>  
       </LazyHero>
     </div>
   );
--- src/components/Second.js
+++ src/components/Second.js
@@ -1,10 +1,24 @@
 import React from 'react';
-// import LazyHero from 'react-lazy-hero';
+import LazyHero from 'react-lazy-hero';
+import './Second.css'
 
 function Second() {
   return (
     <div className="Second">
-            <h1>Second Banner</h1>
+      <LazyHero  opacity="0" minHeight="600px"  parallaxOffset="0" imageSrc={ require('../images/hemp-sport.jpg') } className="hero-second" isCentered="true" >
+        <div className="flex-container">
+          <div className="mobile-image">
+            <img src={ require('../images/hemp-sport-mobile.jpg') }/>
+          </div>
+          <div className="copy">
+              <h1>Lotions,</h1>
+              <h1>Extracts,</h1>
+              <h1>Soft gels</h1>
+              <h1>and more.</h1> 
+            <button type="button" onclick="window.location.href='/page2'" >Shop Now</button>
+          </div>
+        </div>  
+      </LazyHero>
     </div>
   );
 }
