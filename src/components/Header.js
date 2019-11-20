c953e7523e72c96a41892989fb397d89c1c40123
Brookton <davidbrookton@gmail.com>
2019-11-20 09:37:04 -0700

    Fixed Cursor on Hover & Mobile Responsiveness

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,10 +5,10 @@ function Header() {
   return (
     <div className="Header">
       <nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <a class="navbar-brand" href="https://bluebirdbotanicals.com">
-      <img className="Logo" src={ require('../images/color-logo.png') } alt="Bluebird Botanicals" aria-label="Bluebird Botanicals"/>
-    </a>
-    </nav>
+        <a class="navbar-brand" href="https://bluebirdbotanicals.com">
+          <img className="Logo" src={ require('../images/color-logo.png') } alt="Bluebird Botanicals" aria-label="Bluebird Botanicals"/>
+        </a>
+      </nav>
     </div>
   );
 }
