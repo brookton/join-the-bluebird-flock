ac0b873732ac53599d1aa190e58072957c7963db
Brookton <davidbrookton@gmail.com>
2019-11-19 20:12:59 -0700

    Corrected mobile issue

--- src/components/Fourth.js
+++ src/components/Fourth.js
@@ -7,9 +7,6 @@ function Fourth(props) {
     <div className="Fourth">
       <LazyHero  opacity="0" minHeight="600px"  parallaxOffset="0" imageSrc={ require('../images/google-fourth.jpg') } className="hero-fourth" isCentered="true" >
       <div className="flex-container-fourth">
-          <div className="mobile-image-fourth">
-            <img src={ require('../images/third-banner-mobile-grf.jpg')}/>
-          </div>
           <div className="copy-fourth">
           <h1>Shop <span className="white">now.</span></h1> 
           <button type="button" onClick={props.redirect} >Begin self care</button>
