f3b58c6f2f97c477429b6f9e199f94f6e6e8b03f
Brookton <davidbrookton@gmail.com>
2019-11-27 09:49:39 -0700

    Created redirect for /products

--- src/components/App.js
+++ src/components/App.js
@@ -1,31 +1,10 @@
 import React from 'react';
-import ReactDOM from "react-dom";
-import { BrowserRouter as Router, Route } from "react-router-dom";
-import { createBrowserHistory } from 'history';
 import Header from './Header'
 import First from './First'
 import Second from './Second'
 import Third from './Third'
 import Fourth from './Fourth'
 import './App.css';
-
-export const history = createBrowserHistory({
-  basename: process.env.PUBLIC_URL
-});
-
-ReactDOM.render(
-  <Router>
-    <div>
-      <Route exact path="/products">
-        <Header />
-        <First redirect={this.redirect}/>
-        <Second redirect={this.redirect}/>
-        <Third redirect={this.redirect}/>
-        <Fourth redirect={this.redirect}/>
-      </Route>
-    </div>
-  </Router>
-);
 class App extends React.Component {
     redirect(){
       window.location.href="https://bluebirdbotanicals.com";
--- src/index.js
+++ src/index.js
@@ -1,7 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import ReactDOM from "react-dom";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import { createBrowserHistory } from 'history';
 import './fonts/CircularStd-Black.otf'
 import './index.css';
 import App from './components/App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+export const history = createBrowserHistory({
+  basename: process.env.PUBLIC_URL
+});
+
+ReactDOM.render(
+  <Router>
+    <div>
+      <Route exact path="/products">
+        <App />
+      </Route>
+    </div>
+  </Router>, document.getElementById('root'))
+) ;
