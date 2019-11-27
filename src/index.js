44918b0ecfabfecf66cac355aa1c1602717144f1
Brookton <davidbrookton@gmail.com>
2019-11-27 10:12:36 -0700

    Removed React Router, Unable to Deploy Base Route

--- src/index.js
+++ src/index.js
@@ -1,21 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter as Router, Route } from "react-router-dom";
-import { createBrowserHistory } from 'history';
 import './fonts/CircularStd-Black.otf'
 import './index.css';
 import App from './components/App';
 
-const customHistory = createBrowserHistory({
-  basename: process.env.PUBLIC_URL
-});
-
-ReactDOM.render(
-  <Router history={customHistory}>
-    <div>
-      <Route path="/products">
-        <App />
-      </Route>
-    </div>
-  </Router>, document.getElementById('root')
-);
+ReactDOM.render(<App />, document.getElementById('root'));
