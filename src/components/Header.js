fc6b8ac4e1e408a1b2c68bd6aa4616784384e5c4
Brookton <davidbrookton@gmail.com>
2019-12-24 12:12:41 -0700

    Changed Logo Alt for SEO

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,7 +6,7 @@ function Header() {
     <div className="Header">
       <nav class="navbar navbar-expand-lg navbar-light bg-light">
         <a class="navbar-brand" href="https://bluebirdbotanicals.com">
-          <img className="Logo" src={ require('../images/color-logo.png') } alt="Logo for Top Hemp Company with Best Hemp Lotions" aria-label="Logo for Top Hemp Company with Best Hemp Lotions"/>
+          <img className="Logo" src={ require('../images/color-logo.png') } alt="top hemp company with the best hemp lotions" aria-label="top hemp company with the best hemp lotions"/>
         </a>
       </nav>
     </div>
